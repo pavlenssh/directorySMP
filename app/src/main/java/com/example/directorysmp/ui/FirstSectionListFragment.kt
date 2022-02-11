6bca3678a48fbe389429a63c7452393592577469
pavlen <pavlenssh@yandex.ru>
2022-02-12 00:18:45 +0300

    Implemented samples chapters into recycler view and a few part of data source

--- app/src/main/java/com/example/directorysmp/ui/FirstSectionListFragment.kt
+++ app/src/main/java/com/example/directorysmp/ui/FirstSectionListFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.directorysmp.databinding.FragmentFirstSectionListBinding
 import com.example.directorysmp.ui.adapters.FirstSectionAdapter
@@ -25,6 +26,7 @@ class FirstSectionListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         recyclerView = binding.recyclerView
+        recyclerView.layoutManager = LinearLayoutManager(this.requireContext())
         recyclerView.adapter = FirstSectionAdapter()
     }
 
--- app/src/main/java/com/example/directorysmp/ui/adapters/FirstSectionAdapter.kt
+++ app/src/main/java/com/example/directorysmp/ui/adapters/FirstSectionAdapter.kt
@@ -1,32 +1,48 @@
 package com.example.directorysmp.ui.adapters
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.directorysmp.R
 
 class FirstSectionAdapter() :
     RecyclerView.Adapter<FirstSectionAdapter.FirstSectionViewHolder>() {
 
-    class FirstSectionViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+    private val chapters: List<String> = listOf(
+        "АНЕСТЕЗИОЛОГИЯ и РЕАНИМАТОЛОГИЯ",
+        "Токсикология",
+        "Токсикология (детская)",
+        "Кардиология"
+    )
 
+    class FirstSectionViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+        val textView = view.findViewById<TextView>(R.id.text_view_item)
     }
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
-    ): FirstSectionAdapter.FirstSectionViewHolder {
-        TODO("Not yet implemented")
+    ): FirstSectionViewHolder {
+        val layout = LayoutInflater
+            .from(parent.context)
+            .inflate(R.layout.item_view, parent, false)
+
+        return FirstSectionViewHolder(layout)
     }
 
     override fun onBindViewHolder(
-        holder: FirstSectionAdapter.FirstSectionViewHolder,
+        holder: FirstSectionViewHolder,
         position: Int
     ) {
-        TODO("Not yet implemented")
+        val item = chapters[position]
+
+        holder.textView.text = item
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return chapters.size
     }
 
 }
